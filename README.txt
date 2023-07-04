c2f16170a7753948319f2b2f5e084ec0d4eb1850
Oleg Sharov <oleg.sharov@shtapel.com>
2023-07-04 15:57:03 +0300

    initial commit

---
+++ README.txt
@@ -0,0 +1,8 @@
+You will need to get apiId and apiHash from https://my.telegram.org.
+npm run i
+node main.js
+Put you phone number that you used to get apiId and apiHash
+Put login code
+This is asked only once, when starting an app. 
+Than it will write session string to file and read it on app start.
+channels.txt - contains usernames to listen messages from
---
+++ main.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const telegram = require('telegram');
+const session = require('telegram/sessions');
+const input = require('input');
+const fs = require('fs');
+const { NewMessage } = require('telegram/events');
+
+const sessionId = fs.existsSync('.session') ? fs.readFileSync('.session').toString() : '';
+const stringSession = new session.StringSession(sessionId);
+
+let channelsText = fs.readFileSync('channels.txt').toString();
+let channels = channelsText.split('\n').map(c => c.trim());
+
+async function eventPrint(event) {
+    const userName  = event.message._sender ? event.message._sender.username : null;
+    const text = event.message.message;
+    if (channels.includes(userName)) {
+        console.log(`@${userName} [${phone}]: ${text}`);
+    }
+}
+
+(async () => {
+    const client = new telegram.TelegramClient(stringSession, 
+        parseInt(process.env.API_ID),
+        process.env.API_HASH, 
+        {connectionRetries: 5});
+
+    await client.start({
+        phoneNumber: async () => await input.text("Please enter your number: "),
+        phoneCode: async () => await input.text("Please enter the code you received: "),
+        onError: (err) => console.log(err),
+    });
+
+    fs.writeFileSync('.session', client.session.save());
+    client.addEventHandler(eventPrint, new NewMessage({}));
+})();
